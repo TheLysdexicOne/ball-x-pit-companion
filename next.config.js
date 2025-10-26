50f6425708a726e51aec2919c973a4df3275c730
TheLysdexicOne <mcarrier63@gmail.com>
2025-10-26 09:21:16 -0400

    more setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true, // GitHub Pages doesn't support Next.js Image Optimization
+  },
+  basePath: '/ball-x-pit-companion',
+  assetPrefix: '/ball-x-pit-companion/',
+};
 
 module.exports = nextConfig;
